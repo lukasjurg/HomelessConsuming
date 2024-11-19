ddd7d3754c08a7d36a1c39c6e86f16dedd8f2b3c
lukasjurg <lukas.jurgutis@stud.viko.lt>
2024-11-19 02:52:42 +0000

    feat: integrate API testing into main class

--- src/main/java/team15/homelessconsuming/HomelessConsumingApplication.java
+++ src/main/java/team15/homelessconsuming/HomelessConsumingApplication.java
@@ -1,7 +1,30 @@
 package team15.homelessconsuming;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import team15.homelessconsuming.client.ApiClient;
+import team15.homelessconsuming.model.ServiceCategory;
+
+@SpringBootApplication
 public class HomelessConsumingApplication {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(HomelessConsumingApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(ApiClient client) {
+        return args -> {
+            System.out.println("Testing API to get all Service Categories...");
+
+            // Get All Service Categories
+            ServiceCategory[] serviceCategories = client.getAllServiceCategories();
+            System.out.println("All Service Categories: ");
+            for (ServiceCategory category : serviceCategories) {
+                System.out.println(category);
+            }
+        };
     }
 }
